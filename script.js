5999e13d0dd5dbee8a0d4f11f1580f9d68103b1d
BonesTheGhost <bonestheghost@gmail.com>
2020-07-13 21:10:56 -0500

    [007-Controls]:: onclick successfully funneling to a single function. Deliberately chose to use multiple onclick listeners with 'getElementById' since using 'querySelectorAll' would require multiple IF branches to check each ID, resulting in slower code. The IF nightmare is coming regardless since I will be using only vanilla javascript at this point, but for now I figure one less series of branched logic means more efficient click reporting. Buttons output to console correctly. Next will test a dummy game loop.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 console.log("[script.js]:: Attached correctly")
 
+/*
+let inspectButton = document.querySelector(".defend-button");
+let inspectButton = document.querySelector(".forward-button");
+let inspectButton = document.querySelector(".attack-button");
+
+let inspectButton = document.querySelector(".left-button");
 let inspectButton = document.querySelector(".inspect-button");
+let inspectButton = document.querySelector(".right-button");
+
+let inspectButton = document.querySelector(".help-button");
+let inspectButton = document.querySelector(".backward-button");
+let inspectButton = document.querySelector(".item-button");
 
 
 
@@ -8,4 +19,40 @@ showSomeText = (chosenButton) => {
   console.log(chosenButton);
 }
 
-inspectButton.onclick() = showSomeText();
+inspectButton.onclick = showSomeText;
+*/
+
+//Grab the onclick event and pass the id of the element clicked.
+document.getElementById("defend-button").onclick = function() {
+  handoffInput(this.id)
+}
+document.getElementById("forward-button").onclick = function() {
+  handoffInput(this.id)
+}
+document.getElementById("attack-button").onclick = function() {
+  handoffInput(this.id)
+}
+document.getElementById("left-button").onclick = function() {
+  handoffInput(this.id)
+}
+document.getElementById("inspect-button").onclick = function() {
+  handoffInput(this.id)
+}
+document.getElementById("right-button").onclick = function() {
+  handoffInput(this.id)
+}
+document.getElementById("help-button").onclick = function() {
+  handoffInput(this.id)
+}
+document.getElementById("backward-button").onclick = function() {
+  handoffInput(this.id)
+}
+document.getElementById("item-button").onclick = function() {
+  handoffInput(this.id)
+}
+
+
+
+function handoffInput(event) {
+  console.log("[Mouse Event]: ", event);
+}
