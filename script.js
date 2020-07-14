b06d2034a599cc41a90522aa50e8f612bd5bcbd3
BonesTheGhost <bonestheghost@gmail.com>
2020-07-13 20:11:15 -0500

    [006-ControlOnClick]:: Trying to utilize the onclick event with my software buttons to simply output to the console repeatedly at this point.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 console.log("[script.js]:: Attached correctly")
 
 let inspectButton = document.querySelector(".inspect-button");
+
+
+
+showSomeText = (chosenButton) => {
+  console.log(chosenButton);
+}
+
+inspectButton.onclick() = showSomeText();
