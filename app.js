3e3cbe55906725b3fe01ebee04ca19cd8b919a9e
BonesTheGhost <bonestheghost@gmail.com>
2020-07-21 17:07:48 -0500

    [012-(0.0.3)]:: Menus >> Both Hamburger menus work on click to open the respective sidebars.

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 console.log("[app.js]:: Attached and working!");
 
+const output = (variable, explanation, variableReturn) =>{
+  console.log("["+variable+"]:: "+explanation+" == "+variableReturn);
+}
+
 //================= CSS Animation Script ===================
+const burgerLeft = document.querySelector('.burger.left');
+const burgerRight = document.querySelector('.burger.right');
+const inventory = document.getElementById('inventorySidebar');
+const stats = document.getElementById('statsSidebar');
+
+//Click events for burger menus
+burgerLeft.addEventListener('click', ()=> {
+  output(burgerLeft.classList,"Left burger menu clicked.", burgerLeft);
+  inventory.classList.toggle('active');
+})
+burgerRight.addEventListener('click', ()=> {
+  output(burgerRight.classList,"Right burger menu clicked.", burgerRight);
+  stats.classList.toggle('active');
+})
 
 //==========================================================
