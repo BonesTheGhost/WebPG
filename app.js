93b281cf7a42d900a98822f8a0f1054c4a6a2992
BonesTheGhost <bonestheghost@gmail.com>
2020-08-30 17:21:21 -0500

    [017-(0.1.7)]:: Menus + Settings Panel >> Added in the code for the sidebar menus to work. Changed styling for the overall font sizes of the desktop version.

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 console.log("[app.js]: Attached and working!")
+
+//================= CSS Animation Script ===================
+
+const burgerLeft = document.querySelector('.burger.left');
+const burgerRight = document.querySelector('.burger.right');
+
+const inventory = document.getElementById('inventorySidebar');
+const stats = document.getElementById('statsSidebar');
+
+const settingsButton = document.getElementById('settingsButton');
+const settingsPanel = document.getElementById('settingsPanel');
+
+let attachBurgerMenus = () => {
+  //Click events for burger menus
+  burgerLeft.addEventListener('click', ()=> {
+    inventory.classList.toggle('active');
+    burgerLeft.classList.toggle('toggle');
+  });
+  burgerRight.addEventListener('click', ()=> {
+    stats.classList.toggle('active');
+    burgerRight.classList.toggle('toggle');
+  });
+};
+
+let attachSettingsPanel = () => {
+  settingsButton.addEventListener('click', ()=> {
+    settingsPanel.classList.toggle('active');
+  })
+}
+
+//==========================================================
+
+attachBurgerMenus();
+attachSettingsPanel();
