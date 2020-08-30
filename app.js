2c30a653d270bc6582b9052266b48118026bf724
BonesTheGhost <bonestheghost@gmail.com>
2020-08-30 17:52:23 -0500

    [018-(0.1.8)]:: Added Close windows behind the menus for easier navigation.

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
 console.log("[app.js]: Attached and working!")
 
-//================= CSS Animation Script ===================
+//================= CSS Panel Animation Scripts ===================
 
 const burgerLeft = document.querySelector('.burger.left');
 const burgerRight = document.querySelector('.burger.right');
 
 const inventory = document.getElementById('inventorySidebar');
+const invClose = document.getElementById('inventoryClose');
+
 const stats = document.getElementById('statsSidebar');
+const statsClose = document.getElementById('statsClose');
 
 const settingsButton = document.getElementById('settingsButton');
 const settingsPanel = document.getElementById('settingsPanel');
+const settingsClose = document.getElementById('settingsClose');
 
 let attachBurgerMenus = () => {
   //Click events for burger menus
   burgerLeft.addEventListener('click', ()=> {
     inventory.classList.toggle('active');
     burgerLeft.classList.toggle('toggle');
+    invClose.classList.toggle('active');
   });
   burgerRight.addEventListener('click', ()=> {
     stats.classList.toggle('active');
     burgerRight.classList.toggle('toggle');
+    statsClose.classList.toggle('active');
   });
 };
 
 let attachSettingsPanel = () => {
   settingsButton.addEventListener('click', ()=> {
     settingsPanel.classList.toggle('active');
+    settingsClose.classList.toggle('active');
   })
 }
 
+//Click on the area outside of inventory to close the sidebar.
+invClose.onclick =()=> {
+  invClose.classList.toggle('active');
+  inventory.classList.toggle('active');
+  burgerLeft.classList.toggle('toggle');
+};
+
+//Click on the area outside of stats to close the sidebar.
+statsClose.onclick =()=> {
+  statsClose.classList.toggle('active');
+  stats.classList.toggle('active');
+  burgerRight.classList.toggle('toggle');
+};
+
+//Click on the area outside of settings panel to close the sidebar.
+settingsClose.onclick =()=> {
+  settingsPanel.classList.toggle('active');
+  settingsClose.classList.toggle('active');
+};
 //==========================================================
 
 attachBurgerMenus();
